a9a56f38470138b32d02ccbe65242304e5b2230f
bill.pace <billpace24@gmail.com>
2022-05-06 09:27:44 -0500

    define Node and Arc structs

---
+++ src/network/arc.rs
@@ -0,0 +1,12 @@
+/// An arc that connects two nodes in the network.
+/// Each arc tracks the ID numbers of the nodes where it starts and ends, the cost associated with
+/// pushing a single unit of flow down the arc, the lower and upper bounds on flow that must/can be
+/// pushed down the arc, and the current amount of flow down the arc.
+struct Arc {
+    start_node: usize,
+    end_node: usize,
+    cost: f32,
+    min_flow: usize,
+    max_flow: usize,
+    current_flow: usize,
+}
---
+++ src/network/node.rs
@@ -0,0 +1,8 @@
+/// A generic node in the network, used to represent source/sink, workers, and tasks.
+/// Each node has an ID number sequentially generated on construction, and a collection of other
+/// ID numbers corresponding to the nodes that it connects to via existing arcs. Note that since
+/// this network is directed, the connected nodes do not point back to this node.
+struct Node {
+    id: usize,
+    connected_nodes: vec<usize>,
+}
