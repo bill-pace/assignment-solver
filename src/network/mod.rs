3d82983bf68111a25ef9c23baf87b509b108df74
bill.pace <billpace24@gmail.com>
2022-05-06 11:13:58 -0500

    move test_push_flow's cost value into a variable instead of multiple magic numbers

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -8,10 +8,11 @@ fn test_push_flow() {
     // setup
     let node_a_id = 65498;
     let node_b_id = 63524657;
+    let cost = 16.8;
     let mut nodes = HashMap::new();
     nodes.insert(node_a_id,node::Node::new(node_a_id));
     nodes.insert(node_b_id, node::Node::new(node_b_id));
-    let mut arc = arc::Arc::new(node_a_id, node_b_id, 16.8,
+    let mut arc = arc::Arc::new(node_a_id, node_b_id, cost,
                                      0, 1, &mut nodes);
 
     // test
@@ -22,7 +23,7 @@ fn test_push_flow() {
     assert_eq!(nodes.get(&node_a_id).unwrap().get_num_connected_nodes(), 0);
     assert_eq!(nodes.get(&node_b_id).unwrap().get_num_connected_nodes(), 1);
     assert_eq!(nodes.get(&node_b_id).unwrap().get_first_connected_node_id(), Some(node_a_id));
-    assert_eq!(arc.get_cost(), -16.8_f32);
+    assert_eq!(arc.get_cost(), -cost);
     assert_eq!(arc.get_start_node_id(), node_b_id);
     assert_eq!(arc.get_end_node_id(), node_a_id)
 }
