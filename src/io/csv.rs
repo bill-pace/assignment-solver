879dcd15351753e35c94d3d59889c4608704d985
bill.pace <billpace24@gmail.com>
2022-05-07 19:47:37 -0500

    implement processing of worker data in CsvReader

--- src/io/csv.rs
+++ src/io/csv.rs
@@ -20,7 +20,12 @@ use crate::network::Network;
 ///     Worker 2 Name | Task 1 Affinity | Task 2 Affinity | Task 3 Affinity | ...
 ///     --------------|-----------------|-----------------|-----------------|----
 ///     ...
+/// Task minima and maxima must be nonnegative integers, and setting the max to 0 will be treated as
+/// 0 rather than as infinite. Affinities can be any 32-bit floating-point value, including negative
+/// numbers, and if left blank will represent an unacceptable assignment (e.g. the worker cannot do
+/// the corresponding task).
 pub struct CsvReader {
+    // TODO: make these vectors of tuples to preserve ordering
     tasks: HashMap<usize, String>,
     workers: HashMap<usize, String>
 }
@@ -30,7 +35,7 @@ impl CsvReader {
         CsvReader { tasks: HashMap::new(), workers: HashMap::new() }
     }
 
-    fn process_file<R>(&mut self, mut reader: R) -> std::io::Result<Network>
+    fn process_file<R>(&mut self, reader: R) -> std::io::Result<Network>
         where R: BufRead {
         let mut network = Network::new();
         let mut line_iter = reader.lines();
@@ -54,15 +59,19 @@ impl CsvReader {
         self.process_tasks(&mut network, task_names, task_minima, task_maxima)?;
 
         // initialize workers
-        // while let Some(line) = line_iter.next() {
-        //
-        // }
+        while let Some(line) = line_iter.next() {
+            match line {
+                Ok(l) => self.process_worker(&mut network, l)?,
+                Err(err) => return Err(err)
+            }
+        }
 
         Ok(network)
     }
 
     fn process_tasks(&mut self, network: &mut Network, task_names: String, task_minima: String,
-                     task_maxima: String) -> Result<(), std::io::Error> {
+                     task_maxima: String) -> std::io::Result<()> {
+        // TODO: handle missing info
         for task_info in zip(task_names.split(","),
                              zip(task_minima.split(","), task_maxima.split(",")))
             .skip(1) {
@@ -83,6 +92,31 @@ impl CsvReader {
             let task_id = network.add_task(minimum, maximum);
             self.tasks.insert(task_id, task_info.0.trim().to_string());
         }
+        Ok(())
+    }
+
+    fn process_worker(&mut self, network: &mut Network, worker_info: String)
+        -> std::io::Result<()> {
+        let mut affinities = Vec::new();
+        let mut info = worker_info.split(",");
+        let worker_name = info.next().unwrap().trim().to_string();
+
+        let mut task_id = 2_usize; // 0 is source, 1 is sink, so for n tasks,
+                                         // task IDs are 2..(n+1)
+        while let Some(val) = info.next() {
+            if val != "" {
+                let aff = match f32::from_str(val) {
+                    Ok(v) => v,
+                    Err(err) => return Err(std::io::Error::new(std::io::ErrorKind::InvalidData,
+                        format!(r#"Expected numeric value for worker affinity, found "{}"; error: {}"#, val, err)))
+                };
+                affinities.push((task_id, aff));
+            }
+            task_id += 1;
+        }
+
+        let worker_id = network.add_worker(&affinities);
+        self.workers.insert(worker_id, worker_name);
         Ok(())
     }
 }
@@ -121,3 +155,5 @@ fn test_read() {
                                                                      .collect());
     assert!((total_cost - 12.5_f32).abs() / 12.5_f32 < 5e-10_f32);
 }
+
+// TODO: test error detection in bad input files
