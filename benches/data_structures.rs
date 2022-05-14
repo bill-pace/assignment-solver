4d9f5b7332bad91542edd3ecc04acb90c2e2c5a5
bill.pace <billpace24@gmail.com>
2022-05-14 15:54:14 -0500

    add benchmark for reading hashmaps vs vectors

--- benches/data_structures.rs
+++ benches/data_structures.rs
@@ -22,5 +22,24 @@ fn hash_map(bench: &mut Bencher) {
     })
 }
 
-benchmark_group!(benches, vector, hash_map);
-benchmark_main!(benches);
+fn read_vector(bench: &mut Bencher) {
+    let mut distances = vec![f32::INFINITY; 5102];
+    bench.iter(|| {
+        let i = distances[2345];
+    })
+}
+
+fn read_hash_map(bench: &mut Bencher) {
+    let mut distances: HashMap<usize, f32> = HashMap::new();
+    for i in 0..5101 {
+        distances.insert(i, f32::INFINITY);
+    }
+
+    bench.iter(|| {
+        let i = distances.get(&2345).unwrap();
+    })
+}
+
+benchmark_group!(write_benches, vector, hash_map);
+benchmark_group!(read_benches, read_vector, read_hash_map);
+benchmark_main!(write_benches, read_benches);
