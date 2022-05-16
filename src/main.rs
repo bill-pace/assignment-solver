dd8b35e6ed075fa9fcf93d0ea288901b2c96e968
bill.pace <billpace24@gmail.com>
2022-05-16 17:55:42 -0500

    move creation of puffin_server out of configurable scope to make sure it isn't dropped before profiling begins

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,12 @@ fn benchmark() { // complete in 4:33 on a 3.8GHz processor
     let mut reader = reader_factory(FileType::CSV);
     let network = reader.read_file("src/benchmarkInput.csv").unwrap();
 
+    let server_addr = format!("{}:{}",
+                              local_ip_address::local_ip().unwrap().to_string(),
+                              puffin_http::DEFAULT_PORT);
+    let _puffin_server = puffin_http::Server::new(&server_addr).unwrap();
     #[cfg(feature = "profiling")]
     {
-        let server_addr = format!("{}:{}",
-                                  local_ip_address::local_ip().unwrap().to_string(),
-                                  puffin_http::DEFAULT_PORT);
-        let _puffin_server = puffin_http::Server::new(&server_addr).unwrap();
         puffin::set_scopes_on(true);
     }
 
