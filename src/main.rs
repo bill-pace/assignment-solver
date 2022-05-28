b990bb82d6b4878729643d086527a448853a5d90
bill.pace <billpace24@gmail.com>
2022-05-28 11:17:39 -0500

    include file picker example from egui/eframe for basis of next steps

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,7 @@
-use std::env;
-use crate::io::{FileType, Reader, Writer, reader_factory, writer_factory};
-
 mod network;
 mod io;
+mod ui;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
-        eprintln!("Usage: {} INPUT_FILENAME OUTPUT_FILENAME", args[0]);
-
-        std::process::exit(1);
-    }
-
-    let mut reader = reader_factory(FileType::CSV);
-    let network = reader.read_file(args[1].to_string()).unwrap();
-    network.find_min_cost_max_flow().unwrap();
-    let writer = writer_factory(FileType::CSV);
-    writer.write_file(&network, args[2].to_string()).unwrap();
+    ui::launch_gui();
 }
